ebc53e5dc2997ceb551d8d9bfde5a501e590cf63
dyCodes <yusufdaudu51@gmail.com>
2023-09-09 21:11:32 +0100

    feat: add profile object

--- app.js
+++ app.js
@@ -6,8 +6,24 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api", (req, res) => {
-	console.log(req.query);
-	res.send(req.query);
+	if (!req.query.slack_name || !req.query.track) {
+		return res.status(400).json({
+			error: "slack_name and track are required parameters",
+		});
+	}
+	// Create a profile object
+	const profile = {
+		slack_name: req.query.slack_name,
+		current_day: new Date().toLocaleDateString("en-US", { weekday: "long" }),
+		utc_time: new Date().toISOString(),
+		track: req.query.track,
+		github_file_url: "https://github.com/dyCodes/stage1-hngx/blob/main/app.js",
+		github_repo_url: "https://github.com/dyCodes/stage1-hngx",
+		status_code: 200,
+	};
+
+	// Return the profile as a JSON object
+	res.json(profile);
 });
 
 app.listen(8000, () => {
